fa19cfcb2effb6f909cb76b60e02f7a84d9effd6
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-06-12 11:11:06 -0700

    added second dummy recipe to test

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif')
+    new Recipe('A test recipe', 'This is simply a test', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif'),
+    new Recipe('Another test recipe', 'This is simply a test x2', 'http://2.bp.blogspot.com/-UXaKfAnAWRY/TbZ7Jtz5g8I/AAAAAAAALbs/0Sz83Nr5oxQ/s1600/recipe+clipart.gif')
   ];
 
   constructor() { }
